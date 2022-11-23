172631d19fb8956d143287033735fd0c304b2059
Aktar uz zaman <auzaman50.cse@gmail.com>
2022-11-23 08:18:27 +0600

    HTML Form

---
+++ process.php
@@ -0,0 +1,37 @@
+<?php
+if (isset($_POST["submit"])) {
+    $firstName = $_POST["first_name"];
+    $lastName  = $_POST["last_name"];
+    $email  = $_POST["email"];
+    $color = $_POST["color"];
+   
+    if (!empty($firstName) && !empty($lastName) && !empty($email) && !empty($color)) {
+        
+        /*host, username, password, dbname*/
+        $link = mysqli_connect("localhost", "root", "", "form");
+        
+        // Check connection
+        if($link === false){
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+        
+        // Attempt insert query execution
+        $sql = "INSERT INTO users (first_name, last_name, email, color) VALUES ('$firstName', '$lastName' , '$email', '$color')";
+        if(mysqli_query($link, $sql)){
+            echo "Records inserted successfully.";
+        } else{
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        }
+        
+        // Close connection
+        mysqli_close($link);
+
+    }else{
+        echo "Please provide all information";
+    }
+}
+else {
+    echo "hello world";
+}
+
+?>
